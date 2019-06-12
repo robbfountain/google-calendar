459f8dc39250d315becb8d6e31b58c4446980566
Robb Fountain <robb@131studios.com>
2019-06-12 16:12:14 -0400

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,7 @@
 Route::group([
     'namespace' => 'onethirtyone\GoogleCalendar\app\Http\Controllers',
     'prefix' => 'calendar',
+    'middleware' => 'web',
 ], function () {
     Route::get('oauth2callback', 'CalendarOauthController@callback')
         ->name('calendar.oauth.callback');
