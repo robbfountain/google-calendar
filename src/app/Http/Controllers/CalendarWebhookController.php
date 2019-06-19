b04edaa4802ff7a3fbc53fd931751bccf64ada8e
Robb Fountain <robb@131studios.com>
2019-06-19 13:04:44 -0400

    Update CalendarWebhookController.php

--- src/app/Http/Controllers/CalendarWebhookController.php
+++ src/app/Http/Controllers/CalendarWebhookController.php
@@ -27,7 +27,7 @@ class CalendarWebhookController
     public function handle(Request $request)
     {
         Log::info('Webhook Received');
-        Log::info(json_encode($request->toArray()));
+        Log::info(json_encode($request->headers()));
     }
 
 }
