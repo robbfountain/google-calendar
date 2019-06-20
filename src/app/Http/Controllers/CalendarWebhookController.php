3ee374a2de0ea6b1929ab08e0b569e45d06fc9bd
Robb Fountain <robb@131studios.com>
2019-06-19 21:41:04 -0400

    Update CalendarWebhookController.php

--- src/app/Http/Controllers/CalendarWebhookController.php
+++ src/app/Http/Controllers/CalendarWebhookController.php
@@ -3,15 +3,14 @@
 namespace onethirtyone\GoogleCalendar\App\Http\Controllers;
 
 use Google_Exception;
-use Illuminate\Contracts\Routing\ResponseFactory;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\URL;
-use onethirtyone\GoogleCalendar\App\Events\GoogleCalendarWebhookFired;
 use Ramsey\Uuid\Uuid;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\RedirectResponse;
 use onethirtyone\GoogleCalendar\Channel;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use onethirtyone\GoogleCalendar\App\Events\GoogleCalendarWebhookFired;
 
 /**
  * Class CalendarWebhookController
