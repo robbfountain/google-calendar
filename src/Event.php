c895f5511f4acf15af9b41e750c0c0853f428da6
Robb Fountain <robb@131studios.com>
2019-08-09 13:31:46 -0400

    Update Event.php

--- src/Event.php
+++ src/Event.php
@@ -136,8 +136,7 @@ class Event
             Client::updateClientWithSyncToken($calendarEvents->getNextSyncToken());
         }
 
-        Log::info(json_encode($calendarEventsCollection));
-        
+
         return static::mapIntoCalendarEvent($calendarEventsCollection, $calendarId);
     }
 
